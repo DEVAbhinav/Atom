e3b108a9ca83b9b2b79636a22033b66d140628c7
Devabhinav <abhinavpandey.1996@gmail.com>
2016-04-16 05:31:44 +0530

    socket succesfully working in 1 channel char message

--- server.js
+++ server.js
@@ -260,14 +260,14 @@
 
 
     // socket programming
-
-    io.on("connection",function (socket) {
+    io.on('connection', function(socket){
         console.log("a user connected");
-        socket.on('chatmessage', function(data){
-              io.emit('chat message', data);
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
-    });
-
+});
+    
+ 
 
 
 
